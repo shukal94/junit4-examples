0130bd33de8475a95953ce9a5194d61e82a8bceb
Benedikt Ritter <beneritter@gmail.com>
2013-02-03 10:48:28 +0100

    Added example for TestName rule

---
+++ src/test/java/de/systemoutprintln/junit4examples/rules/TestNameRuleTest.java
@@ -0,0 +1,35 @@
+package de.systemoutprintln.junit4examples.rules;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+public class TestNameRuleTest {
+	
+	@Rule
+	public TestName testName = new TestName();
+
+	@Before
+	public void setUp() {
+		System.out.println(String.format("Starting %s...", testName.getMethodName()));
+	}
+
+	@After
+	public void tearDown() {
+		System.out.println(String.format("Done with %s!", testName.getMethodName()));
+		
+	}
+	
+	@Test
+	public void aTest() {
+		System.out.println(String.format("Running %s...", testName.getMethodName()));
+	}
+
+	@Test
+	public void anotherTest() {
+		System.out.println(String.format("Running %s...", testName.getMethodName()));
+	}
+
+}
